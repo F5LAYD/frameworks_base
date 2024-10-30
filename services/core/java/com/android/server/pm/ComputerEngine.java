55434bae00308443fcbad5a88073d68e045d71dd
minaripenguin <minaripenguin@users.noreply.github.com>
2024-10-30 14:53:03 +0800

    services: Introduce QuickSwitch feature [1/2]
    * initial feature rework
    * add support for pixel launcher
    * adapt for a16
    
    Co-authored-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- services/core/java/com/android/server/pm/ComputerEngine.java
+++ services/core/java/com/android/server/pm/ComputerEngine.java
@@ -163,6 +163,8 @@ import com.android.server.utils.WatchedSparseBooleanArray;
 import com.android.server.utils.WatchedSparseIntArray;
 import com.android.server.wm.ActivityTaskManagerInternal;
 
+import org.rising.server.QuickSwitchService;
+
 import libcore.util.EmptyArray;
 
 import java.io.BufferedOutputStream;
@@ -1003,6 +1005,8 @@ public class ComputerEngine implements Computer {
             HideAppListUtils.shouldHideAppList(mContext, packageName)) {
             return null;
         }
+        if (QuickSwitchService.shouldHide(userId, packageName))
+            return null;
         return getApplicationInfoInternal(packageName, flags, Binder.getCallingUid(), userId);
     }
 
@@ -1020,6 +1024,8 @@ public class ComputerEngine implements Computer {
             HideAppListUtils.shouldHideAppList(mContext, packageName)) {
             return null;
         }
+        if (QuickSwitchService.shouldHide(userId, packageName))
+            return null;
         flags = updateFlagsForApplication(flags, userId);
 
         if (!isRecentsAccessingChildProfiles(Binder.getCallingUid(), userId)) {
@@ -1729,6 +1735,8 @@ public class ComputerEngine implements Computer {
             HideAppListUtils.shouldHideAppList(mContext, packageName)) {
             return null;
         }
+        if (QuickSwitchService.shouldHide(userId, packageName))
+            return null;
         return getPackageInfoInternal(packageName, PackageManager.VERSION_CODE_HIGHEST,
                 flags, Binder.getCallingUid(), userId);
     }
@@ -1852,7 +1860,7 @@ public class ComputerEngine implements Computer {
         enforceCrossUserPermission(callingUid, userId, false /* requireFullPermission */,
                 false /* checkShell */, "get installed packages");
 
-        return recreatePackageList(callingUid, mContext,
+        return QuickSwitchService.recreatePackageList(callingUid, mContext,
                         userId, getInstalledPackagesBody(flags, userId, callingUid));
     }
 
@@ -4956,7 +4964,7 @@ public class ComputerEngine implements Computer {
             }
         }
 
-        return recreateApplicationList(callingUid, mContext, userId, list);
+        return QuickSwitchService.recreateApplicationList(callingUid, mContext, userId, list);
     }
 
     @Nullable
