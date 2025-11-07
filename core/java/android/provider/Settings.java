e9e6547a19d86d0f370983e824b7333a83e1e13c
cjybyjk <cjybyjk@zjnu.edu.cn>
2025-11-07 23:24:45 +0100

    base: Show Chinese lunar calendar in QS [1/2]
    [nurkeinneid] updated to android 16
    
    Co-authored-by: NurKeinNeid <nurkeinneid@derpfest.org>
    Change-Id: Ie8187746f17c9a6773305e8c24e642c84dc81685
    Signed-off-by: cjybyjk <cjybyjk@zjnu.edu.cn>
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7131,6 +7131,12 @@ public final class Settings {
         public static final String TINT_STATUSBAR_ICONS_WITH_ACCENT = "tint_statusbar_icons_with_accent";
 
         /**
+         * Whether to show Chinese lunar calendar in the quick settings header.
+         * @hide
+         */
+        public static final String QS_SHOW_LUNAR_CALENDAR = "qs_show_lunar_calendar";
+
+        /**
          * Ramping ringer total duration in seconds
          * @hide
          */
